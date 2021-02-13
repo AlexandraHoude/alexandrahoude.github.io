8b5285dc080b0258d8dce05e361d39036cecb44b
Alexandra <a.houde-pelletier@hotmail.com>
2021-02-13 12:22:17 -0500

    mes premiers composants

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home,
+    Header,
+    Footer,
   }
 }
 </script>
